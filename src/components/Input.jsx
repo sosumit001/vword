f6672baf89bc0b579f66634bc5014f1fb6f526e0
sosumit001 <sosumit001@gmail.com>
2022-10-27 22:50:10 +0530

    t-prog.. bar

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,5 +1,7 @@
 
-import React from "react";
+import React, { useState } from "react";
+import { useRef } from "react";
+
 
 
 
@@ -8,14 +10,28 @@ import './Input.css';
 
 
 const Input = (props) => {
-
+    const [tLen,setTLen] = useState(0);
+    const tele = useRef();
     // useState-1 inorder to store numbers
-   
-
+    function handleChangeEves(e){
+        let len = e.target.value.length;
+        (len >= 1)? tele.current.style.transform = "scale(1)":
+        tele.current.style.transform = "scale(0)"
+        if(len <= 250)
+        {
+            setTLen(len);
+        }
+        
+        
+    }
+  
     return (
         <>
         <div id="addAfterEffect">
-        <textarea ref={props.inRef} spellCheck="false" placeholder="write......"   id={props.inputId} ></textarea>
+        <textarea maxLength={250} onChange={handleChangeEves} ref={props.inRef} spellCheck="false" placeholder="write......"   id={props.inputId} ></textarea>
+        <div ref={tele} id="textScale">
+        <span>{tLen}/250</span>
+        </div>
         </div>
         </>
     )
